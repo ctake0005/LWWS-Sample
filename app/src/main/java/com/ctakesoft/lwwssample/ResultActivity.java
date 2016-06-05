4ea48f05ace3f8edf2fc09ab28bde169a1a77934
takenach <takenach@gmail.com>
2016-06-06 01:28:12 +0900

    Load the image using the Picasso.

--- app/src/main/java/com/ctakesoft/lwwssample/ResultActivity.java
+++ app/src/main/java/com/ctakesoft/lwwssample/ResultActivity.java
@@ -1,15 +1,19 @@
 package com.ctakesoft.lwwssample;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ctakesoft.lwwssample.model.Forecast;
 import com.ctakesoft.lwwssample.net.ForecastPublisher;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 
@@ -55,13 +59,31 @@ public class ResultActivity extends AppCompatActivity implements ForecastPublish
         TextView description = (TextView) mForecastLayout.findViewById(R.id.description_text);
         description.setText(forecast.getDescription().getText());
 
+        TypedArray typedArray = getResources().obtainTypedArray(R.array.daily_panel_array);
+        for (int i = 0, size = forecast.getForecasts().size(); i < size; i++) {
+            LinearLayout layout = (LinearLayout) findViewById(typedArray.getResourceId(i, 0));
+            TextView dailyLabel = (TextView) layout.findViewById(R.id.daily_label);
+            dailyLabel.setText(getResources().getStringArray(R.array.daily_labels)[i]);
+            TextView dailyText = (TextView) layout.findViewById(R.id.daily_text);
+            dailyText.setText(forecast.getForecasts().get(i).getTelop());
+
+            // Load the image using the Picasso.
+            ImageView dailyImage = (ImageView) layout.findViewById(R.id.daily_image);
+            Picasso.with(this).load(forecast.getForecasts().get(i).getImage().getUrl()).into(dailyImage);
+
+            layout.setVisibility(View.VISIBLE);
+        }
         mForecastLayout.setVisibility(View.VISIBLE);
+
+        typedArray.recycle();
     }
 
     @Override
     public void onError(IOException e) {
         mProgressBar.setVisibility(View.GONE);
         mRetryButton.setVisibility(View.VISIBLE);
+
+        Toast.makeText(this, "Network error !!", Toast.LENGTH_SHORT).show();
     }
 
     private void request() {
